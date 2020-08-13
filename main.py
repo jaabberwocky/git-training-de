72d232e0d9b90ae06dfbcafff3a073a99f1a2168
tobias <tobias@dsaid.gov.sg>
2020-08-13 10:42:28 +0800

    Add code file

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+
+
+def main():
+    """
+    Main function.
+    """
+    path_list = os.listdir(os.getcwd())
+    print("asdjflkasjfksdajfkdsjfkdasjfkdasjfkdasjfkasdjfkasdjfaksdjfksadfasdfjadsk")  # noqa: E501
+    print(f'{path_list}')
+    print(f'{add(1,2)}')
+
+
+def add(x, y):
+    """
+    Adds two integers together.
+
+    Args:
+        x (int): First integer
+        y (int): Second integer
+
+    Returns:
+        int: Result of adding x + y
+    """
+    result = x + y
+    return result
+
+
+if __name__ == "__main__":
+    main()
